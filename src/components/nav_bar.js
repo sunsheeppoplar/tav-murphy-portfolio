11baf8832ac5699c0fcd7bc8d1cac120bbdfc27f
Abner Yang <abtyang@gmail.com>
2017-09-04 04:17:22 -0400

    issue #2

---
+++ src/components/nav_bar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends Component {
+	render() {
+		return(
+			<div className="nav-bar">
+				<h1 className="nav-bar__header">Tav Murphy</h1>
+				<h4 className="nav-bar__subheader">Director of Photogrphy</h4>
+				<div>
+					<Link to="/" className="nav-bar__link">Work</Link>
+					<Link to="/about" className="nav-bar__link">About</Link>
+					<Link to="/contact" className="nav-bar__link">Contact</Link>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './components/app';
+import About from './components/about';
+import Contact from './components/contact';
+import Home from './components/home';
+import NavBar from './components/nav_bar';
 
 ReactDOM.render(
-  <App />, document.querySelector('.container')
+	<BrowserRouter>
+		<div>
+			<NavBar />
+			<Switch>
+				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
+				<Route path="/" component={Home} />
+			</Switch>
+		</div>
+	</BrowserRouter>
+  , document.querySelector('.container')
 )
