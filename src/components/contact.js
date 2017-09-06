37e5c4c235588d97e2c471ddcd590d5c6e8bf45c
Abner Yang <abtyang@gmail.com>
2017-09-06 03:10:30 -0400

    issue #7 - created contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Contact extends Component {
 	render() {
 		return(
-			<div>
-				Contact
+			<div className="contact">
+				<p><a href="mailto:tavmurphy@gmail.com">tavmurphy@gmail.com</a></p>
+				<p>(561) 000-0000</p>
 			</div>
 		);
 	}
