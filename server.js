d1abfd102ab172ac8236009fcf220ebed6b6ad93
Abner Yang <abtyang@gmail.com>
2017-10-24 03:16:11 -0400

    #8 adding additional babel dependencies for heroku deploy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'))
+	res.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 app.listen(port);
