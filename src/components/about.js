6cc64cf2d1546c2d6c6bf8db368484919f70cdd8
Abner Yang <abtyang@gmail.com>
2017-11-01 03:29:43 -0400

    #6 added assets directory, finished laying out about page

--- src/components/about.js
+++ src/components/about.js
@@ -4,21 +4,25 @@ class About extends Component {
 	render() {
 		return(
 			<div className="about">
-				<div className="about__copy">
-					Arma virumque cano, Troiae qui primus ab oris
-					Italiam, fato profugus, Laviniaque venit
-					litora, multum ille et terris iactatus et alto
-					vi superum saevae memorem Iunonis ob iram;
-					multa quoque et bello passus, dum conderet urbem,
-					inferretque deos Latio, genus unde Latinum,
-					Albanique patres, atque altae moenia Romae.
+				<div className="about__info">
+					<div className="about__info__copy">
+						Arma virumque cano, Troiae qui primus ab oris
+						Italiam, fato profugus, Laviniaque venit
+						litora, multum ille et terris iactatus et alto
+						vi superum saevae memorem Iunonis ob iram;
+						multa quoque et bello passus, dum conderet urbem,
+						inferretque deos Latio, genus unde Latinum,
+						Albanique patres, atque altae moenia Romae.
 
-					Musa, mihi causas memora, quo numine laeso,
-					quidve dolens, regina deum tot volvere casus
-					insignem pietate virum, tot adire labores
-					impulerit. Tantaene animis caelestibus irae?
+						Musa, mihi causas memora, quo numine laeso,
+						quidve dolens, regina deum tot volvere casus
+						insignem pietate virum, tot adire labores
+						impulerit. Tantaene animis caelestibus irae?
+					</div>
+					<div className="about__info__image">
+						<img src="../assets/tm_about.jpg"></img>
+					</div>
 				</div>
-				<div className="about__image"></div>
 			</div>
 		);
 	}
