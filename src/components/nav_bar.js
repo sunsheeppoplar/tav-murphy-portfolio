e944a08c3f0780bd3a3b234c0d53c1e043350924
Abner Yang <abtyang@gmail.com>
2017-11-02 05:02:13 -0400

    closes #2 links now change colors when component is active

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 class NavBar extends Component {
 	render() {
@@ -8,9 +8,9 @@ class NavBar extends Component {
 				<h1 className="nav-bar__header"><Link to="/">TAVNER MURPHY</Link></h1>
 				<h4 className="nav-bar__subheader">Director of Photogrphy</h4>
 				<div>
-					<Link to="/" className="nav-bar__link">Work</Link>
-					<Link to="/about" className="nav-bar__link">About</Link>
-					<Link to="/contact" className="nav-bar__link">Contact</Link>
+					<NavLink to="/" activeClassName="nav-bar__link--selected" exact className="nav-bar__link">Work</NavLink>
+					<NavLink to="/about" activeClassName="nav-bar__link--selected" exact className="nav-bar__link">About</NavLink>
+					<NavLink to="/contact" activeClassName="nav-bar__link--selected" exact className="nav-bar__link">Contact</NavLink>
 				</div>
 			</div>
 		);
