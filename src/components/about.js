14829c510f8a5a997c049262a713dd350b3084fb
Abner Yang <abtyang@gmail.com>
2018-01-21 16:47:44 -0500

    #6 spaced paragraphs in about page

--- src/components/about.js
+++ src/components/about.js
@@ -6,13 +6,15 @@ class About extends Component {
 			<div className="about wrapper">
 				<div className="about__info">
 					<div className="about__info__copy">
-						Tavner is a Director of Photography and Camera Operator based in New York City. His work has
-						screened at film festivals around the country and world. A backpacker, photographer, and music
-						junkie, he thrives on telling stories through visual imagery and highlighting the unconventional,
-						the uncomfortable, and the extraordinary.
-						<br />
-						Past Clients include: Dropbox, VICE Media, i-D, Red Bull, Omaze, DreamworksTV, Okayplayer, American
-						Eagle,  Complex, True Entertainment, The Salvation Army
+						<p>
+							Tavner is a Director of Photography and Camera Operator based in New York City. His work has
+							screened at film festivals around the country and world. A backpacker, photographer, and music
+							junkie, he thrives on telling stories through visual imagery and highlighting the
+							unconventional, the uncomfortable, and the extraordinary.
+						</p>
+						<p>
+							Past Clients include: Dropbox, VICE Media, i-D, Red Bull, Omaze, DreamworksTV, Okayplayer, American Eagle,  Complex, True Entertainment, The Salvation Army
+						</p>
 					</div>
 				<div className="about__info__image">
 					<img src="../assets/tm_about.jpg"></img>
