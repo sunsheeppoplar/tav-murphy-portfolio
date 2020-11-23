6f545ccde257b49be4151c5b7211c13db5dd7ba8
Abner Yang <abtyang@gmail.com>
2020-11-22 19:15:06 -0500

    Adjust Current City Copy, Add New Photos
    - adjusts margin-top to be even from the top of the nav for copy and photos
    - #25

--- src/components/About/InfoImage.js
+++ src/components/About/InfoImage.js
@@ -4,7 +4,8 @@ class AboutInfoImage extends Component {
 	render() {
 		return(
 			<div>
-				<img src="../assets/tm_about.jpg" className="about__info__image"></img>
+				<img src="../assets/tm_about_1.jpg" className="about__info__image"></img>
+				<img src="../assets/tm_about_2.jpg" className="about__info__image"></img>
 			</div>
 		)
 	}
