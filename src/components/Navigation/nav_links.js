90ffbe6db51aaa9901963cca2ad667973a20d755
Abner Yang <abtyang@gmail.com>
2020-12-06 23:22:51 -0500

    Update Nav Link from "Work" to "Selected Work"
    - refactored mobile and desktop components to pull from a common data file
    - changes file name to be within folder heiarachy
    - keeping snake case to be consistent for now, though might want to change to camel case convention in future PR
    - closes #30

---
+++ src/components/Navigation/nav_links.js
@@ -0,0 +1,16 @@
+const navLinks = [
+    {
+        "label": "Selected Work",
+        "to": "/"
+    },
+    {
+        "label": "About",
+        "to": "/about"
+    },
+    {
+        "label": "Contact",
+        "to": "/contact"
+    }
+];
+
+export default navLinks;
